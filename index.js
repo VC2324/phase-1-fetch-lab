85a302c5be26e64e27f0dadf439df35d47c5f885
Victor <victorac2324@gmail.com>
2024-03-16 15:56:05 -0400

    used fetch request to grap api data then fed that dat to another function to post

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch ("https://anapioficeandfire.com/api/books")
+.then (response => response.json()) 
+.then (response => {
+  renderBooks(response)
+
   
-}
+})
+};
+
+
+// }
+
+// .then(jsonData => {
+//   renderBooks(jsonData);
+// });
+  // To pass the tests, don't forget to return your fetch!
+
+
+// fetch("https://anapioficeandfire.com/api/books")
+//   .then((resp) => resp.json())
+//   .then((json) => console.log(json));
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +33,28 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+// function fetchBooks() {
+//   return fetch("https://anapioficeandfire.com/api/books")
+//     .then (response => response.json()) 
+//     .then (data => {
+//       return data
+//     })
+// };
+
+// function renderBooks() {
+//   const books = fetchBooks()
+
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   renderBooks();
+// });
